5c3159094b3db396a44aaf0f7422590747c2380c
sokonana <23A054Q@mymail.nyp.edu.sg>
2023-12-24 23:20:45 +0800

    Update Scripts and Test Samples

--- eval.sh
+++ eval.sh
@@ -1,8 +1,8 @@
 export CUDA_VISIBLE_DEVICES="-1"
-# MODEL=ssd_mobilenet_v2_320x320_coco17_tpu-8
-MODEL=centernet_resnet50_v1_fpn_512x512_coco17_tpu-8
+MODEL=ssd_mobilenet_v2_320x320_coco17_tpu-8
+# MODEL=centernet_resnet50_v1_fpn_512x512_coco17_tpu-8
 PROJ=it107_assignment
-EXPERIMENT=run3
+EXPERIMENT=run4
 PIPELINE_CONFIG_PATH=/home/sokonana/${PROJ}/models/${MODEL}/${EXPERIMENT}/pipeline.config
 MODEL_DIR=/home/sokonana/${PROJ}/models/${MODEL}/${EXPERIMENT}/
 CHECKPOINT_DIR=/home/sokonana/${PROJ}/models/${MODEL}/${EXPERIMENT}/
--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-# MODEL=ssd_mobilenet_v2_320x320_coco17_tpu-8
-MODEL=centernet_resnet50_v1_fpn_512x512_coco17_tpu-8
+MODEL=ssd_mobilenet_v2_320x320_coco17_tpu-8
+# MODEL=centernet_resnet50_v1_fpn_512x512_coco17_tpu-8
 PROJ=it107_assignment
-EXPERIMENT=run3
+EXPERIMENT=run4
 CUDA_VISIBLE_DEVICES=0
 PIPELINE_CONFIG_PATH=/home/sokonana/${PROJ}/models/${MODEL}/${EXPERIMENT}/pipeline.config
 MODEL_DIR=/home/sokonana/${PROJ}/models/${MODEL}/${EXPERIMENT}/
